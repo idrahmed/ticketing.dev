794c34e6ffc37394ec7da67af2fe7c7dcb4a9f0b
idrah07 <idrisahmed987@gmail.com>
2022-03-15 08:06:50 +1100

    added comment to orders ticket-created-listener

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -9,6 +9,7 @@ import { queueGroupName } from './queue-group-name'
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
     subject: Subjects.TicketCreated = Subjects.TicketCreated
     queueGroupName = queueGroupName
+    // replicating newly created ticket.
     async onMessage(data: TicketCreatedEvent['data'], msg: Message) {   
         const { title, price, id } = data
         const ticket = Ticket.build({
