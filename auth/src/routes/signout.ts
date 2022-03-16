69322f2009933962009d48b4207e98d14d6841e0
idrah07 <idrisahmed987@gmail.com>
2022-03-16 20:17:52 +1100

    ed

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const router = express.Router()
 
 router.post('/api/users/signout', (req, res) => {
-    // we'll set the session obj on the req obj to null, removing any user details
+    // we'll set the session obj on the req obj to null, removing any user details.
     req.session = null
 
     res.send({})
