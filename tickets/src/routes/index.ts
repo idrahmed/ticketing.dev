966abfc34075bfcb4d37e2b0611cc951e69a49be
idrah07 <idrisahmed987@gmail.com>
2022-03-16 17:08:54 +1100

    .

--- tickets/src/routes/index.ts
+++ tickets/src/routes/index.ts
@@ -4,7 +4,7 @@ import { Ticket } from "../models/ticket";
 const router = express.Router()
 
 router.get('/api/tickets', async(req: Request, res: Response) => {
-    // only returning tickets that are not reserved i.e dont have and orderId. 
+    // only returning tickets that are not reserved i.e dont have an orderId. 
     const tickets = await Ticket.find({orderId: undefined})
 
     res.send(tickets)
