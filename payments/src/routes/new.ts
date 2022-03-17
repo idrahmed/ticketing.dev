498496915a42fa70a4c9e2d0d58f508f4da455b1
idrah07 <idrisahmed987@gmail.com>
2022-03-17 18:52:46 +1100

    added comments

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -16,6 +16,7 @@ import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
+// we require the user to be logged in, the stripe token from the browser and an orderId
 router.post(
   "/api/payments",
   RequireAuth,
@@ -23,24 +24,28 @@ router.post(
   validateRequest,
   async (req: Request, res: Response) => {
     const { token, orderId } = req.body;
-
+    // find the order thats being bought
     const order = await Order.findById(orderId);
     if (!order) {
       throw new NotFoundError();
     }
+    // make sure the userId on the order is equal to the incoming userId.
     if (order.userId !== req.currentUser?.id) {
       throw new NotAuthorizedError();
     }
+    // if the order status is already expired or cancelled, then throw error. 
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError("Order is expired/cancelled");
     }
 
+    // create the stripe charge
     const charge = await stripe.charges.create({
       currency: "usd",
       amount: order.price * 100,
       source: token,
     });
 
+    // include this payment in the db
     const payment = Payment.build({
       orderId,
       stripeId: charge.id,
@@ -48,6 +53,7 @@ router.post(
 
     await payment.save();
 
+    // now publish that this order is paid for and completed. 
     new PaymentCreatedPublisher(natsWrapper.client).publish({
       id: payment.id,
       orderId: payment.orderId,
