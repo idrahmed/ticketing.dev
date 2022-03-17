f12354bcb5f717e89c3bf29a33e74a14bbedf8af
idrah07 <idrisahmed987@gmail.com>
2022-03-17 17:58:43 +1100

    change returned tickets

--- tickets/src/routes/index.ts
+++ tickets/src/routes/index.ts
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.get('/api/tickets', async(req: Request, res: Response) => {
     // only returning tickets that are not reserved i.e dont have an orderId. 
-    const tickets = await Ticket.find({orderId: undefined})
+    const tickets = await Ticket.find({orderId: undefined}) || {}
 
     res.send(tickets)
 })
