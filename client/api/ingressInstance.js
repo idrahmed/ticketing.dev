7b98653f5f7e547daca1e1bbde46ad16f639fa31
idrah07 <idrisahmed987@gmail.com>
2022-03-17 13:46:48 +1100

    .

--- client/api/ingressInstance.js
+++ client/api/ingressInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const ingressInstance = ({ req }) => {
   return axios.create({
-    baseURL: "http://ticketing-prod.me/",
+    baseURL: "http://ticketing-prod.me",
     headers: req.headers,
   });
 };
